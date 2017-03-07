2a89a67e2899950a05990cf55b63f55a66d85d67
Indra Lukmana <indra.lukmana.il@gmail.com>
2017-03-07 10:26:01 +0000

    Make a script from linechart js that show a stacked linechart

---
+++ js/payment_stackedlinechart.js
@@ -0,0 +1,34 @@
+// Date Dimension
+var dateDimension = payments.dimension(function(d) {
+    return d.date;
+});
+
+// Date group with total
+var dateGroup = dateDimension.group().reduceSum(function(d) {
+    return d.total;
+});
+
+// Date group with tip
+var dateGroupTip = dateDimension.group().reduceSum(function(d) {
+    return d.tip;
+})
+
+// Minimum and maximum value for dateGroup
+var minDate = dateDimension.bottom(1)[0].date;
+var maxDate = dateDimension.top(1)[0].date;
+
+// define the barchart for #totalchart
+var lineChart = dc.lineChart("#lineChart")
+.width(1360)
+.height(200)
+.margins({top: 10, bottom: 30, right: 10, left: 70})
+.renderHorizontalGridLines(true)
+.renderArea(true)
+.dimension(dateDimension)
+.group(dateGroup, "Total Spend")
+.stack(dateGroupTip, "Tip")
+.legend(dc.legend().x(1200).y(5).itemHeight(12).gap(5))
+.x(d3.time.scale().domain([minDate,maxDate]))
+
+lineChart.yAxis().ticks(5);
+lineChart.xAxis().ticks(4);
