283d2a8d57a94194822d4bdf341ef7349dc6e5c0
Indra Lukmana <indra.lukmana.il@gmail.com>
2017-03-07 06:45:32 +0000

    Devine minimum and maximum date value

--- js/payment_linechart.js
+++ js/payment_linechart.js
@@ -6,13 +6,17 @@ var dateDimension = payments.dimension(function(d) {
 // Date group
 var dateGroup = dateDimension.group();
 
+// Minimum and maximum value for dateGroup
+var minDate = dateDimension.bottom(1)[0].date;
+var maxDate = dateDimension.top(1)[0].date;
+
 // define the barchart for #totalchart
 var lineChart = dc.lineChart("#lineChart")
 .width(1360)
 .height(200)
 .dimension(dateDimension)
 .group(dateGroup)
-.x(d3.scale.linear().domain([0,400]))
+.x(d3.date.scale.domain([minDate,maxDate]))
 .xUnits(dc.units.fp.precision(100));
 
 lineChart.yAxis().ticks(5);
