a67f753ea4c5287b15c251cade4ba7978cab72d1
Indra Lukmana <indra.lukmana.il@gmail.com>
2017-03-08 09:13:06 +0000

    Include the script in html and remove unused piechart definitions

--- js/payment_piechart.js
+++ js/payment_piechart.js
@@ -13,9 +13,4 @@ dc.pieChart("#paymentPieChart")
     .width(1365)
     .height(200)
     .dimension(typeDimension)
-    .group(typeGroup)
-    .x(d3.scale.ordinal().domain(['visa', 'cash', 'tab']))
-    .xUnits(dc.units.ordinal)
-    .barPadding(0.2)
-    .outerPadding(0)
-    .yAxis().ticks(5);
+    .group(typeGroup);
