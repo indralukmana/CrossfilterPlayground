f30586e855aa6baa118228afe932fe46f5972710
Indra Lukmana <indra.lukmana.il@gmail.com>
2017-03-08 09:37:27 +0000

    Change the doughnout chart color scheme

--- js/payment_doughnoutchart.js
+++ js/payment_doughnoutchart.js
@@ -20,4 +20,5 @@ dc.pieChart("#paymentDoughnoutChart")
     .renderLabel(false)
     .radius(100)
     .innerRadius(80)
-    .legend(dc.legend().x(1200).y(5).itemHeight(12).gap(5));
+    .legend(dc.legend().x(1200).y(5).itemHeight(12).gap(5))
+    .colors(d3.scale.category10());
