f255d6e245ddf0cda8a46382c601aaea02f0cc0b
Indra Lukmana <indra.lukmana.il@gmail.com>
2017-03-08 09:07:24 +0000

    Refactor remove unused codes

--- js/payment_piechart.js
+++ js/payment_piechart.js
@@ -8,13 +8,8 @@ var typeGroup = typeDimension.group().reduceSum(function (d) {
     return d.total;
 });
 
-// tipDimension
-var tipDimension = payments.dimension(function(d) {
-    return d.tip;
-});
-
-// Define the barchart for #chart
-dc.barChart("#paymentChart")
+// Define the piechart for #paymentPieChart
+dc.pieChart("#paymentPieChart")
     .width(1365)
     .height(200)
     .dimension(typeDimension)
@@ -24,25 +19,3 @@ dc.barChart("#paymentChart")
     .barPadding(0.2)
     .outerPadding(0)
     .yAxis().ticks(5);
-
-// Total dimension
-var totalDimension = payments.dimension(function(d) {
-  return d.total;
-});
-
-// Total group
-var totalGroup = totalDimension.group(function (d) {
-    return Math.floor(d/100)*100;
-});
-
-// define the barchart for #totalchart
-var totalChart = dc.barChart("#totalChart")
-.width(1360)
-.height(200)
-.dimension(totalDimension)
-.group(totalGroup)
-.x(d3.scale.linear().domain([0,400]))
-.xUnits(dc.units.fp.precision(100));
-
-totalChart.yAxis().ticks(5);
-totalChart.xAxis().ticks(4);
