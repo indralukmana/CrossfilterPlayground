a82576ee1be220d8f7f407942ca666329048d9be
Indra Lukmana <indra.lukmana.il@gmail.com>
2017-03-08 09:32:17 +0000

    Initialize doughnoutchart script and insert to the html

---
+++ js/payment_doughnoutchart.js
@@ -0,0 +1,22 @@
+// Type Dimension
+var typeDimension = payments.dimension(function(d) {
+    return d.type;
+});
+
+// Type group with Total from typeDimension
+var typeGroup = typeDimension.group().reduceSum(function (d) {
+    return d.total;
+});
+
+// Define the piechart for #paymentPieChart
+dc.pieChart("#paymentDoughnoutChart")
+    .width(1365)
+    .height(200)
+    .dimension(typeDimension)
+    .group(typeGroup)
+    .title(function(d) {
+        return d.key + ': £' + d.value;
+    })
+    .label(false)
+    .radius(100)
+    .innerRadius(80);
