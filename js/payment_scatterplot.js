ead353758ddaa7418700fdcdbd33d8dd5a31b5c4
Indra Lukmana <indra.lukmana.il@gmail.com>
2017-03-09 23:41:02 +0000

    Modified the style for the scatterplot

--- js/payment_scatterplot.js
+++ js/payment_scatterplot.js
@@ -12,4 +12,11 @@ var scatter = dc.scatterPlot("#paymentScatterPlot")
 .height(200)
 .dimension(scatterDimension)
 .group(scatterGroup)
+.symbolSize(20)
+.clipPadding(25)
+.colorAccessor(function(d) {
+    return d.value;
+})
+.symbol('triangle-down')
+.colors(d3.scale.category20b())
 .x(d3.scale.linear().domain([0,300]));
