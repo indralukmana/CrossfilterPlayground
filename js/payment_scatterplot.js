b3d2865fbb86b3e6e9938226dd04a54a0d3f1601
Indra Lukmana <indra.lukmana.il@gmail.com>
2017-03-09 22:55:40 +0000

    Initialize scatter plot
    made the div for scatterplot in html and duplicate linechart to make
    the scatterplot

---
+++ js/payment_scatterplot.js
@@ -0,0 +1,27 @@
+// Date Dimension
+var dateDimension = payments.dimension(function(d) {
+    return d.date;
+});
+
+// Date group
+var dateGroup = dateDimension.group().reduceSum(function(d) {
+    return d.total;
+});
+
+// Minimum and maximum value for dateGroup
+var minDate = dateDimension.bottom(1)[0].date;
+var maxDate = dateDimension.top(1)[0].date;
+
+// define the scatterplot for #paymentScatterPlot
+var lineChart = dc.lineChart("#paymentScatterPlot")
+.width(1360)
+.height(200)
+.margins({top: 10, bottom: 30, right: 10, left: 70})
+.renderHorizontalGridLines(true)
+.renderArea(true)
+.dimension(dateDimension)
+.group(dateGroup)
+.x(d3.time.scale().domain([minDate,maxDate]))
+
+lineChart.yAxis().ticks(5);
+lineChart.xAxis().ticks(4);
