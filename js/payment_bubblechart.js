f03640d2b8d6fbcadcd351b98a2982228d8fbcc4
Indra Lukmana <indra.lukmana.il@gmail.com>
2017-03-10 00:09:43 +0000

    Modify the bubble chart

--- js/payment_bubblechart.js
+++ js/payment_bubblechart.js
@@ -10,13 +10,32 @@ var scatterGroup = scatterDimension.group();
 var bubblechart = dc.bubbleChart("#paymentBubbleChart")
 .width(1360)
 .height(200)
+.margins({top:40, bottom:60, right:80, left:60})
 .dimension(scatterDimension)
 .group(scatterGroup)
-.symbolSize(20)
-.clipPadding(25)
+.renderHorizontalGridLines(true)
+.renderVerticalGridLines(true)
+.clipPadding(75)
 .colorAccessor(function(d) {
     return d.value;
 })
-.symbol('triangle-down')
-.colors(d3.scale.category20b())
+.colors(d3.scale.category10())
+.keyAccessor(function(d) {
+  return d.key[0];
+})
+.valueAccessor(function(d) {
+  return d.key[1];
+})
+.radiusValueAccessor(function(d) {
+  return d.value;
+})
+.maxBubbleRelativeSize(0.05)
+.title(function(d) {
+  return 'x: ' + d.key[0] + ', y: ' + d.key[1] + ', val: ' + d.value;
+})
+.yAxisLabel("Tip Size")
+.r(d3.scale.linear().domain([1,6]))
+.y(d3.scale.linear().domain([0,200]))
 .x(d3.scale.linear().domain([0,300]));
+
+bubblechart.yAxis().ticks(5);
