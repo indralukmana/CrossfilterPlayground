31095b8ca00242958831580fb1d81668deeacf93
Indra Lukmana <indra.lukmana.il@gmail.com>
2017-03-10 07:24:06 +0000

    Add bootstrap and modify the data table to use bootstrap

--- js/payment_datatable.js
+++ js/payment_datatable.js
@@ -8,7 +8,20 @@ var paymentDataTable = dc.dataTable("#paymentDataTable")
     .width(1360)
     .height(300)
     .dimension(dateDimension)
+    .showGroups(false)
+    .size(50)
     .group(function(d) {
-        return d;
+        return d.type;
     })
-    .columns(['date','quantity','total','tip','type']);
+    .columns([{label: 'Time', format: function(d) { return d.date.getHours() +":" + d.date.getMinutes(); }},
+              'quantity',
+              'total',
+              'tip',
+              'type'])
+    .sortBy(function(d) {
+        return d.type;
+    })
+    .order(d3.ascending)
+    .on("renderlet", function(table) {
+        table.selectAll('.dc-table-group').classed('info', true);
+    });
