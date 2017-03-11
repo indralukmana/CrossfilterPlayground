ae481e928e9981681fa80ff26237eef8ff5fa259
Indra Lukmana <indra.lukmana.il@gmail.com>
2017-03-11 20:35:46 +0000

    Define the grouping for stacked barchart

--- js/payment_stackedbarchart.js
+++ js/payment_stackedbarchart.js
@@ -4,21 +4,41 @@ var typeDimension = payments.dimension(function(d) {
 });
 
 // Type group with Total from typeDimension
-var typeGroup = typeDimension.group().reduceSum(function (d) {
-    return d.total;
-});
+// var typeGroup = typeDimension.group().reduceSum(function (d) {
+//     return d.total;
+// });
+
+// Type group that return object
+var typeGroup = typeDimension.group().reduce(reduceAdd, reduceRemove, reduceInitial);
 
 // tipDimension
 var tipDimension = payments.dimension(function(d) {
     return d.tip;
+
 });
+
+function reduceAdd(i,d) {
+  i[d.quantity] = (i[d.quantity]||0) + d.total;
+  return i;
+}
+
+function reduceRemove(i,d) {
+  i[d.quantity] = (i[d.quantity]||0) - d.total;
+  return i;
+}
+
+function reduceInitial() {
+  return {};
+}
 
 // Define the barchart for #chart
 dc.barChart("#paymentStackedBarchart")
     .width(1365)
     .height(200)
     .dimension(typeDimension)
-    .group(typeGroup)
+    .group(typeGroup, "group1", function(d){ return d.value[1] || 0; })
+    .stack(typeGroup, "group2", function(d){ return d.value[2]; })
+    .legend(dc.legend().x(1100).y(10).itemHeight(15).gap(5))
     .x(d3.scale.ordinal().domain(['visa', 'cash', 'tab']))
     .xUnits(dc.units.ordinal)
     .barPadding(0.2)
