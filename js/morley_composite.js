ef17682820954124f5e7877ed1c47f8d9aa7dd9a
Indra Lukmana <indra.lukmana.il@gmail.com>
2017-03-12 20:30:21 +0000

    Define the dimension for composite chart

--- js/morley_composite.js
+++ js/morley_composite.js
@@ -9,14 +9,14 @@ d3.csv("morley.csv", function(error, experiments) {
     var facts = crossfilter(experiments);
 
     var runDimension = facts.dimension(function(d) {
-      return [d.Expt, d.Run];
+      return [d.Run];
     });
 
     var runGroup = runDimension.group().reduceSum(function(d) {
-      return d.Speed;
+      return d.Speed/5;
     });
 
-    var series = dc.seriesChart("#morleyComposite")
+    var compositeChart = dc.compositeChart("#morleyComposite")
         .width(1360)
         .height(400)
         .margins({
@@ -25,21 +25,8 @@ d3.csv("morley.csv", function(error, experiments) {
             right: 80,
             left: 60
         })
-        .chart(function(cht) {
-          return dc.lineChart(cht).renderArea(true).interpolate('basis');
-        })
         .dimension(runDimension)
         .group(runGroup)
-        .keyAccessor(function(d) {
-          return d.key[1];
-        })
-        .valueAccessor(function(d) {
-          return d.value;
-        })
-        .seriesAccessor(function(d) {
-          return d.key[0]
-        })
-        .legend(dc.legend().x(100).y(200).itemHeight(13).gap(5).horizontal(2).legendWidth(1360).itemWidth(70))
         .x(d3.scale.linear().domain([1,20]));
 
 
