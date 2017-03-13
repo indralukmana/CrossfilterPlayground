969bf1b0f76d8d7fd52be96f053223889899a94a
Indra Lukmana <indra.lukmana.il@gmail.com>
2017-03-13 08:34:08 +0000

    change the projection to mercator

--- js/vc_choropleth.js
+++ js/vc_choropleth.js
@@ -20,13 +20,22 @@ d3.csv("vc.csv", function(errorVC, vcData) {
         return d.Raised;
     })
 
+
     d3.json("us-states.json", function(errorJson, statesJson) {
+
+      // centering the map
+      var centerMap = d3.geo.centroid(statesJson);
+
+      // mercator map projection
+      var projectionMercator = d3.geo.mercator().center(centerMap).scale(500).translate([200,100]);
+
 
         usChart
             .width(1360)
             .height(500)
             .dimension(states)
             .group(stateRaisedSum)
+            .projection(projectionMercator)
             .colors(d3.scale.quantize().range(["#E2F2FF", "#C4E4FF", "#9ED2FF", "#81C5FF", "#6BBAFF", "#51AEFF", "#36A2FF", "#1E96FF", "#0089FF", "#0061B5"]))
             .colorDomain([0, 200])
             .colorCalculator(function(d) {
