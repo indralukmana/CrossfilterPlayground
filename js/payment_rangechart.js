7548a82e499c10be3ac0a7d1e40694084336f981
Indra Lukmana <indra.lukmana.il@gmail.com>
2017-03-14 10:31:57 +0000

    edit rangechart

--- js/payment_rangechart.js
+++ js/payment_rangechart.js
@@ -8,20 +8,34 @@ var dateGroup = dateDimension.group().reduceSum(function(d) {
     return d.total;
 });
 
+// Volume group
+var volGroup = dateDimension.group();
+
 // Minimum and maximum value for dateGroup
 var minDate = dateDimension.bottom(1)[0].date;
 var maxDate = dateDimension.top(1)[0].date;
 
-// define the barchart for #totalchart
-var lineChart = dc.lineChart("#lineChart")
-.width(1360)
-.height(200)
-.margins({top: 10, bottom: 30, right: 10, left: 70})
-.renderHorizontalGridLines(true)
-.renderArea(true)
-.dimension(dateDimension)
-.group(dateGroup)
-.x(d3.time.scale().domain([minDate,maxDate]))
+var volGroup = dc.barChart("#rangeChart");
 
-lineChart.yAxis().ticks(5);
-lineChart.xAxis().ticks(4);
+volgroup
+  .height(70)
+  .width(1360)
+  .margins({top: 10, bottom: 30, right: 10, left: 70})
+
+  .dimension(dateDimension)
+  .group(volGroup)
+  .x(d3.time.scale().domain([minDate,maxDate]))
+
+// define the barchart for #totalchart
+// var lineChart = dc.lineChart("#rangeChart")
+// .width(1360)
+// .height(200)
+// .margins({top: 10, bottom: 30, right: 10, left: 70})
+// .renderHorizontalGridLines(true)
+// .renderArea(true)
+// .dimension(dateDimension)
+// .group(dateGroup)
+// .x(d3.time.scale().domain([minDate,maxDate]))
+//
+// lineChart.yAxis().ticks(5);
+// lineChart.xAxis().ticks(4);
